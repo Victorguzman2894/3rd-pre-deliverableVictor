60c052814ddf6544b0775fed897e1418c2fad3a9
Victor Guzman <guzmangironvictor@gmail.com>
2023-03-20 18:52:18 -0300

    updating 3rd deliverable with models and database

--- getting-started/my_football_team/atletico_de_madrid/models.py
+++ getting-started/my_football_team/atletico_de_madrid/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class TeamCategory(models.Model):
+    team_name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Players(models.Model):
+    team = models.ForeignKey(TeamCategory, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    age = models.CharField(max_length=200)
+    intake_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
